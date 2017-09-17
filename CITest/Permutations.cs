2bdd6590500e3b27544e9e808384aca8cee563b1
zonai <zonai@ZONAID-SURFACE>
2017-09-17 19:47:09 +1000

    Add project files.

---
+++ CITest/Permutations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITest
+{
+    public class Permutations
+    {
+        public static long Compute(long n)
+        {
+            var result = n;
+            if (n>1)
+            {
+                result = result * Compute(result - 1);
+            }
+            return result;
+        }
+    }
+}
---
+++ TestPermutations/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CITest;
+namespace TestPermutations
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Perms_should_Match()
+        {
+            var result = Permutations.Compute(4);
+            Assert.IsTrue(result==24);
+        }
+    }
+}
